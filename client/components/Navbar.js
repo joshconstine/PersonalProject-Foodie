c7ae24475000c0311dbb105f6da515bc349a7517
joshua constine <joshua.constine97@gmail.com>
2022-03-09 16:31:38 -0600

    navbar is now SFC

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,52 +1,43 @@
 import React from "react";
-import { connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { logout } from "../store";
 import DropDownMenu from "./DropDownMenu";
 import { Button } from "@material-ui/core";
 
-const Navbar = ({ handleClick, isLoggedIn }) => (
-  <div>
-    <h1>Foodie</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <span className="navItem">
-            <DropDownMenu />
-          </span>
+const Navbar = () => {
+  const isLoggedIn = useSelector((state) => state.auth.id);
+  const dispatch = useDispatch();
+  async function handleLogout() {
+    await dispatch(logout());
+  }
 
-          <a className="navItem">
-            <Button onClick={handleClick}>Logout</Button>
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
-);
+  return (
+    <div>
+      <h1>Foodie</h1>
+      <nav>
+        {isLoggedIn ? (
+          <div>
+            {/* The navbar will show these links after you log in */}
+            <span className="navItem">
+              <DropDownMenu />
+            </span>
 
-/**
- * CONTAINER
- */
-const mapState = (state) => {
-  return {
-    isLoggedIn: !!state.auth.id,
-  };
+            <a className="navItem">
+              <Button onClick={handleLogout}>Logout</Button>
+            </a>
+          </div>
+        ) : (
+          <div>
+            {/* The navbar will show these links before you log in */}
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Sign Up</Link>
+          </div>
+        )}
+      </nav>
+      <hr />
+    </div>
+  );
 };
 
-const mapDispatch = (dispatch) => {
-  return {
-    handleClick() {
-      dispatch(logout());
-    },
-  };
-};
-
-export default connect(mapState, mapDispatch)(Navbar);
+export default Navbar;
